27556db1a26813b5fc865ddd60b75b6450dd35b5
Conrad <cang688@aucklanduni.ac.nz>
2026-03-10 14:28:30 +1300

    began working on the slot machine, created the deposit and get-number-of-lines definitions

---
+++ main.py
@@ -0,0 +1,36 @@
+MAX_LINES = 3
+
+
+def deposit():
+    while True:
+        amount = input("Enter deposit Amount: $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than zero.")
+        else:
+            print("Please enter a valid number.")
+
+    return amount
+
+def get_number_of_lines():
+    while True:
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines")
+        else:
+            print("Please enter a valid number.")
+
+    return lines
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+
+main()
